7e8ca7ad64ab6dfca6993bb0aa6a73e5ab8feb0d
varun.arya <arun@antiersolutions.com>
2023-09-21 20:30:47 +0530

    qw

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ app.use(express.json())
 const set = require("./set"); 
 
 
-app.post("/tx", async (req, res) => {
+app.post("/", async (req, res) => {
 
     try {
         const result = await set.factoryCreatetx(req,res)
         res.status(200).json(result)
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: error });
     }
 
 })
